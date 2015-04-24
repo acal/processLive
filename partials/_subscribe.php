5be82e6291994e9f972d60b65c96e29d06cf781c
Adam <adamcalihman@gmail.com>
2015-04-24 07:59:18 -0600

    mailchimp form tweaks
    Made adjustments to mailchimp default HTML and inline CSS to improve
    responsivity for xs.

--- partials/_subscribe.php
+++ partials/_subscribe.php
@@ -1,34 +1,37 @@
+<div class="container">
+<div class="row">
+ 
 <!-- Begin MailChimp Signup Form -->
 <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
 <style type="text/css">
-	#mc_embed_signup{background:inherit; clear:left; width: 400px; font:14px Helvetica,Arial,sans-serif; }
+	#mc_embed_signup{background:inherit; clear:left; width: 300px; font:14px Helvetica,Arial,sans-serif; }
 	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
 	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
 </style>
 <div id="mc_embed_signup" class="center-block">
-<form action="//processsutilities.us10.list-manage.com/subscribe/post?u=4c4c85231a21cffb5dd577d3e&amp;id=2523429788" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<h2 class="text-center">Subscribe to our mailing list</h2>
-<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-<div class="mc-field-group">
-	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-</label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-</div>
-<div class="mc-field-group">
-	<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
-</label>
-	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
-</div>
-<div class="mc-field-group">
-	<label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
-</label>
-	<input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
-</div>
-	<div id="mce-responses" class="clear">
+    <form action="//processsutilities.us10.list-manage.com/subscribe/post?u=4c4c85231a21cffb5dd577d3e&amp;id=2523429788" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+    	<h2 class="text-center">Subscribe to our mailing list</h2>
+    <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+    <div class="mc-field-group">
+    	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+        </label>
+    	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+    </div>
+    <div class="mc-field-group">
+    	<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
+        </label>
+    	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+    </div>
+    <div class="mc-field-group">
+	    <label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
+        </label>
+	    <input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
+    </div>
+    <div id="mce-responses" class="clear">
 		<div class="response" id="mce-error-response" style="display:none"></div>
 		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;"><input type="text" name="b_4c4c85231a21cffb5dd577d3e_2523429788" tabindex="-1" value=""></div>
     <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
     </div>
@@ -36,3 +39,5 @@
 </div>
 <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 <!--End mc_embed_signup-->
+</div>
+</div>
