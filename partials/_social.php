d476a32f96a42bf4764aa3090ecd987e0118c0ba
Adam <adamcalihman@gmail.com>
2015-04-28 14:59:04 -0600

    updates to social href

--- partials/_social.php
+++ partials/_social.php
@@ -3,10 +3,10 @@
     <div class="row">
       <div class="btn-group-vertical" role="group" aria-label="...">
         <a href="mailto:info@processutilites.com" class="btn btn-default" role="button"><i class="icon icon-envelope"></i> Email</a>
-        <a href="#" class="btn btn-default" role="button" target="_blank"><i class="icon icon-facebook-square"></i> Follow</a>
-        <a href="#" class="btn btn-default" role="button" target="_blank"><i class="icon icon-linkedin-square"></i> Follow</a>
-        <a href="#" class="btn btn-default" role="button" target="_blank"><i class="icon icon-facebook"></i> Share</a>
-        <a href="#" class="btn btn-default" role="button" target="_blank"><i class="icon icon-linkedin"></i> Share</a>
+        <a href="https://www.facebook.com/ProcessUtilities" class="btn btn-default" role="button" target="_blank"><i class="icon icon-facebook-square"></i> Follow</a>
+        <a href="https://www.linkedin.com/company/processutilities" class="btn btn-default" role="button" target="_blank"><i class="icon icon-linkedin-square"></i> Follow</a>
+        <a href="http://www.facebook.com/sharer/sharer.php?u=https://www.facebook.com/ProcessUtilities&title=ProcessUtilities Excel Add-in for Engineers" class="btn btn-default" role="button" target="_blank"><i class="icon icon-facebook"></i> Share</a>
+        <a href="http://www.linkedin.com/shareArticle?mini=true&url=[URL]&title=[TITLE]&source=[SOURCE/DOMAIN]" class="btn btn-default" role="button" target="_blank"><i class="icon icon-linkedin"></i> Share</a>
         <a href="https://github.com/ProcessUtilities/documentation" class="btn btn-default" role="button" target="_blank"><i class="icon icon-github"></i></i> Documentation</a>
     </div>
     </div>
