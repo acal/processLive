84c395bf3e52becbb25bf70a66b0a21cda58a461
Adam <adamcalihman@gmail.com>
2015-04-28 10:36:29 -0600

    update purchase button
    change text and add gumroad url

--- partials/_purchaseButton.php
+++ partials/_purchaseButton.php
@@ -1,3 +1,3 @@
   <br>
-  <button type="button" class="btn btn-p1 center-block">Purchase and Download Coming Soon</button>
+  <a href="https://gumroad.com/l/ngIdt" class="btn btn-p1 center-block" role="button">Pre-order Now</a>
   <br>
