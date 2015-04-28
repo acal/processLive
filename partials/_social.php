0d15a26bc9d8d5c800e7c9f23e73f9fec15bfd81
Adam <adamcalihman@gmail.com>
2015-04-28 13:49:16 -0600

    updated href targets

--- partials/_social.php
+++ partials/_social.php
@@ -3,10 +3,10 @@
     <div class="row">
       <div class="btn-group-vertical" role="group" aria-label="...">
         <a href="mailto:info@processutilites.com" class="btn btn-default" role="button"><i class="icon icon-envelope"></i> Email</a>
-        <a href="#" class="btn btn-default" role="button"><i class="icon icon-facebook-square"></i> Follow</a>
-        <a href="#" class="btn btn-default" role="button"><i class="icon icon-linkedin-square"></i> Follow</a>
-        <a href="#" class="btn btn-default" role="button"><i class="icon icon-facebook"></i> Share</a>
-        <a href="#" class="btn btn-default" role="button"><i class="icon icon-linkedin"></i> Share</a>
-        <a href="https://github.com/ProcessUtility/documentation" class="btn btn-default" role="button"><i class="icon icon-github"></i></i> Documentation</a>
+        <a href="#" class="btn btn-default" role="button" target="_blank"><i class="icon icon-facebook-square"></i> Follow</a>
+        <a href="#" class="btn btn-default" role="button" target="_blank"><i class="icon icon-linkedin-square"></i> Follow</a>
+        <a href="#" class="btn btn-default" role="button" target="_blank"><i class="icon icon-facebook"></i> Share</a>
+        <a href="#" class="btn btn-default" role="button" target="_blank"><i class="icon icon-linkedin"></i> Share</a>
+        <a href="https://github.com/ProcessUtility/documentation" class="btn btn-default" role="button" target="_blank"><i class="icon icon-github"></i></i> Documentation</a>
     </div>
     </div>
