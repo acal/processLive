35991ecdfb030325fdca152bbdcd66a6cce64041
Adam <adamcalihman@gmail.com>
2015-05-26 16:45:51 -0600

    added php logic
    "pre-order" will flip to "buy now" after 6/1/15.
    Also deleted unnessessary file.

--- partials/_nav.php
+++ partials/_nav.php
@@ -2,7 +2,7 @@
       <div class="navbar-header">
         <a class="navbar-brand" href="index.php">
           <span itemprop="publisher" itemscope itemtype="http://schema.org/Organization">
-<span itemprop="name">PROCESS | UTILITIES</span></span>
+          <span itemprop="name">PROCESS | UTILITIES</span></span>
         </a>
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
           <span class="sr-only">Toggle navigation</span>
@@ -13,10 +13,24 @@
       </div>
      <div class="navbar-collapse collapse" id="myNavbar">
         <ul class="nav navbar-nav navbar-right">
-          <li><a itemprop="url" data-toggle="collapse" data-target=".navbar-collapse" href="index.php"><span class="sr-only">Toggle navigation</span><i class="icon icon-home"></i></a></li>
+          <li><a data-toggle="collapse" data-target=".navbar-collapse" href="index.php"><span class="sr-only">Toggle navigation</span><i class="icon icon-home"></i></a></li>
           <li><a data-toggle="collapse" data-target=".navbar-collapse" href="#func">functions</a></li>
           <li><a data-toggle="collapse" data-target=".navbar-collapse" href="#consulting">consulting</a></li>
-          <li><a data-toggle="collapse" data-target=".navbar-collapse" href="https://gumroad.com/l/ngIdt" target="_blank">preorder</a></li>
+          <li><a itemprop="downloadUrl" data-toggle="collapse" data-target=".navbar-collapse" 
+          href="https://gumroad.com/l/ngIdt" target="_blank">
+          <?php
+            // $now = new DateTime();
+            $dtA = new DateTime('05/14/2015 8:00AM');
+            $dtB = new DateTime('06/01/2015 8:00AM');
+            
+            if ( $dtA > $dtB ) {
+              echo 'buy';
+            }
+            else {
+              echo 'pre-order';
+            }
+          ?>
+          </a></li>
           <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-search"></span>
             <span class="sr-only">Search dropdown</span>
--- partials/_purchaseButton.php
+++ partials/_purchaseButton.php
@@ -1,6 +1,19 @@
    <div class="row order-button-row">
      <br>
-    <a itemprop="downloadUrl" href="https://gumroad.com/l/ngIdt" class="btn btn-p1 center-block" role="button">Pre-order Now</a>
+    <a itemprop="downloadUrl" href="https://gumroad.com/l/ngIdt" class="btn btn-p1 center-block" role="button">
+        <?php
+            // $now = new DateTime();
+            $dtA = new DateTime('05/14/2015 8:00AM');
+            $dtB = new DateTime('06/01/2015 8:00AM');
+            
+            if ( $dtA > $dtB ) {
+              echo 'Buy Now';
+            }
+            else {
+              echo 'Pre-order Now';
+            }
+        ?>
+        </a>
      <br>
   </div>
  
